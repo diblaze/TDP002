9aa63fc679cf3016321ee3ae13ddbd66dc552230
Denis I. Blazevic <denicevap@gmail.com>
2016-09-07 12:59:30 +0200

    Solitaire encrypt 100%

--- 3/3.2.2.py
+++ 3/3.2.2.py
@@ -1,18 +1,67 @@
 #! /usr/bin/env python3
 import re
-import imp
+import keystream
 
 
 def strip_down_to_AZ(string_to_strip):
+    """Makes a string to uppercase and strips the string of non-alphabet characters"""
+    # only allow A-Z characters
     pattern = "[A-Z]"
+    # cast all letters to uppercase.
     string_to_strip = str(string_to_strip).upper()
-    return re.findall(pattern, string_to_strip)
+    # return all letters found.
+    return "".join(re.findall(pattern, string_to_strip))
+
+
+def convert_to_numbers(string_to_convert):
+    """Converts A-Z strings to numbers."""
+    numbers_from_string = []
+    for letter in string_to_convert:
+        numbers_from_string.append(keystream.letters_to_keys_list[letter])
+    return numbers_from_string
+
+def convert_to_letters(list_to_convert):
+    """Converts 0-9 lists to letters."""
+    final_string = ""
+    for number in list_to_convert:
+        final_string += keystream.keys_to_letters_list[number]
+    return final_string
+
+def sum_numbers_from_lists(list1, list2):
+    temp_list = []
+    for i in range(len(list1)):
+        temp_num = list1[i] + list2[i]
+        if temp_num > 26:
+            temp_num -= 26
+        temp_list.append(temp_num)
+    return temp_list
 
 if __name__ == "__main__":
-    open_file,file_name,descripion = imp.find_module("3.2.1.py")
-    imp.load_module("3.2.2.py", open_file, file_name,descripion)
-    #ask for string to encrypt
+    # ask for string to encrypt
     string_to_encrypt = input("Input string to encrypt: ")
+    # strip string
+    string_to_encrypt = strip_down_to_AZ(string_to_encrypt)
 
-    #only allow A-Z characters
-    print(strip_down_to_AZ(string_to_encrypt))
+    # get keystream
+    encrypt_key = keystream.solitaire_keystream(len(string_to_encrypt))
+
+    # convert string to numbers
+    numbers_from_string = convert_to_numbers(string_to_encrypt)
+    print(numbers_from_string)
+
+    # convert encrypt_key to numbers
+    encrypt_key_numbers = convert_to_numbers(encrypt_key)
+    print(encrypt_key_numbers)
+
+    # sum the numbers list with each other
+    # numbers_from_string = [1,5,3]
+    # encrypt_key_numbers = [3,6,2]
+    # this gives us (1+3),(5+6),(3,2)
+    # if number is higher than 26, then remove 26 from number.
+    list_of_summed_numbers = sum_numbers_from_lists(numbers_from_string, encrypt_key_numbers)
+    print (list_of_summed_numbers)
+    #print (keystream.keys_to_letters_list[17])
+
+    #convert the final summed list to letters
+    encrypted_string = convert_to_letters(list_of_summed_numbers)
+    print(encrypted_string)
