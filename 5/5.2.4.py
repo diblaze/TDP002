7cd2c4f0395b50595996500870daa6540841b702
Denis I. Blazevic <denicevap@gmail.com>
2016-09-28 14:38:32 +0200

    5.2.4 done

--- 5/5.2.4.py
+++ 5/5.2.4.py
@@ -1,34 +1,72 @@
-import os
+import os, sys
 
 
 def execute(cmd, args):
-    commands = ["pwd", "cd", "ls", "cat"]
-    if cmd in commands:
-        if args:
-            if cmd == "cd":
-                return cd(args)
-            elif cmd == "cat":
-                return cat(args)
-        else:
-            if cmd == "pwd":
-                return pwd()
-            elif cmd == "ls":
-                return ls()
+    """
+        Executes the right function
+    """
+    if args:
+        if cmd == "cd":
+            return cd(args)
+        elif cmd == "cat":
+            return cat(args)
     else:
-        return "No such command"
+        if cmd == "pwd":
+            print(pwd())
+        elif cmd == "ls":
+            return ls()
+        elif cmd == "exit":
+            return exit()
+        else:
+            return "No such command"
+
+def exit():
+    """
+        Exits the shell
+    """
+    sys.exit(0)
+
+
+def cat(filename):
+    """
+        Displays the content of a file.
+    """
+    filename = filename[0]
+    with open(filename) as f:
+        for line in f:
+            print(line, end="")
+
+def ls():
+    """
+        Lists all files in the current directory.
+    """
+    list_of_files = [obj for obj in os.listdir(pwd())]
+    for item in list_of_files:
+        if os.path.isdir(pwd() + "/"  + item):
+            index_of_item = list_of_files.index(item)
+            list_of_files[index_of_item] = list_of_files[index_of_item] + "/"
+            print(list_of_files[index_of_item])
+            continue
+
+        print(item)
 
 
 def pwd():
     """
         Prints out the current working directory.
     """
-    print(os.getcwd())
+    return os.getcwd()
 
 def cd(path):
+    if len(path) > 1:
+        return "Command 'c' only takes in one argument!"
+
+    path = path[0]
     try:
         os.chdir(path)
+        pwd()
     except:
-        print("There is no such directory!")
+        return "No such directory!"
 
 
 def main():
